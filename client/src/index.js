c648f2d2dbf347b4a41b9d06d60d97b7881542d4
mj-unosquare <maryjane.mcbride@unosquare.com>
2019-07-22 14:09:22 +0100

    pull request changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import css from "./style.css";
-import styled from "styled-components";
+import { Theme } from "./theme";
+import styled, { createGlobalStyle } from "styled-components";
 
 const Title = styled.h1`
   font-size: 1.5em;
@@ -12,9 +12,16 @@ const Logo = styled.img`
   width: 150px;
 `;
 
+const Body = createGlobalStyle`
+  body {
+    background-color: ${Theme.color.lightgreen};
+  }
+`;
+
 const App = () => {
   return (
     <div>
+      <Body />
       <Logo src={require("./img/icon.png")} alt="plant-logo"></Logo>
       <Title>grow.</Title>
     </div>
