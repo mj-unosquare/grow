7be588bc6de6accbd4315c48b64faef01b0b02cf
mj-unosquare <maryjane.mcbride@unosquare.com>
2019-07-23 10:24:41 +0100

    code review changes to theme and globalstyle

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+
+import { GlobalStyle } from "./styled";
+
+const App = () => {
+  return (
+    <div>
+      <GlobalStyle />
+      <img src={require("./img/icon.png")} alt="plant-logo"></img>
+      <h1>grow.</h1>
+    </div>
+  );
+};
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,31 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Theme } from "./theme";
-import styled, { createGlobalStyle } from "styled-components";
+import App from "./App";
 
-const Title = styled.h1`
-  font-size: 1.5em;
-  color: ${Theme.color.mediumbrown};
-`;
+const app = <App />;
 
-const Logo = styled.img`
-  width: 150px;
-`;
-
-const Body = createGlobalStyle`
-  body {
-    background-color: ${Theme.color.lightgreen};
-  }
-`;
-
-const App = () => {
-  return (
-    <div>
-      <Body />
-      <Logo src={require("./img/icon.png")} alt="plant-logo"></Logo>
-      <Title>grow.</Title>
-    </div>
-  );
-};
-
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(app, document.getElementById("root"));
