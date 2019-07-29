d9d7b553999feb2c99a87c61a15fe03827364f4d
mj-unosquare <maryjane.mcbride@unosquare.com>
2019-07-29 16:35:35 +0100

    added tests for plants and locations services

--- server/src/locations/locations.service.spec.ts
+++ server/src/locations/locations.service.spec.ts
@@ -7,6 +7,7 @@ import { Location } from './locations.entity';
 
 describe('LocationsService', () => {
   let service: LocationsService;
+  let repo: Repository<Location>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -17,9 +18,39 @@ describe('LocationsService', () => {
       .compile();
 
     service = module.get<LocationsService>(LocationsService);
+    repo = module.get<Repository<Location>>(getRepositoryToken(Location));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it(`getAccounts`, async () => {
+    const result: Location = {
+      location_id: 1,
+      location_name: 'plant name',
+      description: 'plant description',
+      imageurl: 'location url',
+      user_id: 1,
+    };
+    jest.spyOn(repo, 'find').mockResolvedValueOnce([result]);
+    expect(await service.getLocationsByUserId(1)).toEqual([result]);
+  });
+
+  it(`getAccount`, async () => {
+    const result: Location = {
+      location_id: 1,
+      location_name: 'plant name',
+      description: 'plant description',
+      imageurl: 'location url',
+      user_id: 1,
+    };
+    jest.spyOn(repo, 'find').mockResolvedValueOnce([result]);
+    expect(await service.getLocation(1)).toEqual([result]);
+  });
+
+  it(`getAccount`, async () => {
+    jest.spyOn(repo, 'find').mockResolvedValueOnce([]);
+    expect(await service.getLocation(1)).toEqual([]);
   });
 });
--- server/src/plants/plants.service.spec.ts
+++ server/src/plants/plants.service.spec.ts
@@ -7,6 +7,7 @@ import { Repository } from 'typeorm';
 
 describe('PlantsService', () => {
   let service: PlantsService;
+  let repo: Repository<Plant>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -17,9 +18,39 @@ describe('PlantsService', () => {
       .compile();
 
     service = module.get<PlantsService>(PlantsService);
+    repo = module.get<Repository<Plant>>(getRepositoryToken(Plant));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it(`getAccounts`, async () => {
+    const result: Plant = {
+      plant_id: 1,
+      plant_name: 'plant name',
+      description: 'plant description',
+      location_id: 1,
+      imageurl: 'plant url',
+    };
+    jest.spyOn(repo, 'find').mockResolvedValueOnce([result]);
+    expect(await service.getPlantsByLocationId(1)).toEqual([result]);
+  });
+
+  it(`getAccount`, async () => {
+    const result: Plant = {
+      plant_id: 1,
+      plant_name: 'plant name',
+      description: 'plant description',
+      location_id: 1,
+      imageurl: 'plant url',
+    };
+    jest.spyOn(repo, 'find').mockResolvedValueOnce([result]);
+    expect(await service.getPlant(1)).toEqual([result]);
+  });
+
+  it(`getAccount`, async () => {
+    jest.spyOn(repo, 'find').mockResolvedValueOnce([]);
+    expect(await service.getPlant(1)).toEqual([]);
   });
 });
